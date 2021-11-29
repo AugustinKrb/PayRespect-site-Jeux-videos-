879c318d3de34a9d8e1b42434a17c945a15efab0
khmsa <93908013+khmsa@users.noreply.github.com>
2021-11-29 09:25:49 +0100

    Rajout de catégorie

--- index.php
+++ index.php
@@ -1,17 +1,61 @@
-<!DOCTYPE html>
+<!doctype html>
+<html lang="fr">
 <head>
-    <title>Pay Request</title>
+  <meta charset="utf-8">
+  <title>Pay Respect</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="script.js"></script>
 </head>
 <body>
-    <header>
 
-    </header>
+<h1>Pay Respect </h1>
 
-    <main>
+<h2>Trouvez plein de jeux sur cet incroyable site qui n’est en fait qu’un prototype réalisé par les bg de BTS SNIR !</h2>
 
-    </main>
+<h3> Les Jeux les plus populaires en ce moment </h3>
 
-    <footer>
 
-    </footeer>
+<p> <ul>
+	<li>Battlefield</li><br>
+	<li>Call of dutry</li><br>
+	<li>Far cry</li>
+	</ul>
+</p>
+ 
+
+ <h3>Les jeux sortis cette semaine </h3>
+
+ <p> <ul>
+	<li>Battlefield1</li><br>
+	<li>Call of dutry2</li><br>
+	<li>Far cry3</li>
+	</ul>
+</p>
+
+
+<h3>Les jeux par categorie par exemple: </h3>
+
+
+ <p> <ul>
+	<li>par date</li><br>
+	<li>par note</li><br>
+	<li>par prix</li>
+	</ul>
+</p>
+
+
+
+
+
+
 </body>
+ <footer>
+ 	
+ <p> <ul>
+	<li>email</li><br>
+	<li>tel num</li><br>
+	<li>@nom d'equipe</li>
+	</ul>
+</p>
+ </footer>
+</html>
