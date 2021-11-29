a3b258ad71a62ddf3b052410a80dbfaa066b629b
AugustinKrb <93907781+AugustinKrb@users.noreply.github.com>
2021-11-29 09:33:21 +0100

    Fermeture balises meta et link

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <head>
         <meta charset="utf-8"/>
         <title>Pay Respect</title>
-        <link rel="stylesheet" href="styles/mainStyle.css">
+        <link rel="stylesheet" href="styles/mainStyle.css"/>
         <script src="script.js"></script>
     </head>
 
