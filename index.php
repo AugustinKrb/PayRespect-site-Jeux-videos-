45ab454f35de554fb7bbf44aadf1dd22074c0847
AugustinKrb <93907781+AugustinKrb@users.noreply.github.com>
2021-11-29 09:31:20 +0100

    Refonte du squelette

--- index.php
+++ index.php
@@ -1,61 +1,54 @@
 <!doctype html>
 <html lang="fr">
-<head>
-  <meta charset="utf-8">
-  <title>Pay Respect</title>
-  <link rel="stylesheet" href="style.css">
-  <script src="script.js"></script>
-</head>
-<body>
 
-<h1>Pay Respect </h1>
+    <head>
+        <meta charset="utf-8">
+        <title>Pay Respect</title>
+        <link rel="stylesheet" href="styles/mainStyle.css">
+        <script src="script.js"></script>
+    </head>
 
-<h2>Trouvez plein de jeux sur cet incroyable site qui n’est en fait qu’un prototype réalisé par les bg de BTS SNIR !</h2>
+    <body>
+        <header>
 
-<h3> Les Jeux les plus populaires en ce moment </h3>
+        </header>
 
+        <main>
+            <h1>Pay Respect </h1>
 
-<p> <ul>
-	<li>Battlefield</li><br>
-	<li>Call of dutry</li><br>
-	<li>Far cry</li>
-	</ul>
-</p>
- 
+            <h2>Trouvez plein de jeux sur cet incroyable site qui n’est en fait qu’un prototype réalisé par les bg de BTS SNIR !</h2>
 
- <h3>Les jeux sortis cette semaine </h3>
+            <h3> Les jeux les plus populaires en ce moment</h3>
 
- <p> <ul>
-	<li>Battlefield1</li><br>
-	<li>Call of dutry2</li><br>
-	<li>Far cry3</li>
-	</ul>
-</p>
+            <ul>
+                <li>Battlefield</li>
+                <li>Call of dutry</li>
+                <li>Far cry</li>
+            </ul>
 
+            <h3>Les jeux sortis cette semaine </h3>
 
-<h3>Les jeux par categorie par exemple: </h3>
+            <ul>
+                <li>Battlefield1</li>
+                <li>Call of dutry2</li>
+                <li>Far cry3</li>
+            </ul>
 
+            <h3>Les jeux par categorie par exemple:</h3>
 
- <p> <ul>
-	<li>par date</li><br>
-	<li>par note</li><br>
-	<li>par prix</li>
-	</ul>
-</p>
+            <ul>
+                <li>par date</li><br>
+                <li>par note</li><br>
+                <li>par prix</li>
+            </ul>
+        </main>
 
-
-
-
-
-
-</body>
- <footer>
- 	
- <p> <ul>
-	<li>email</li><br>
-	<li>tel num</li><br>
-	<li>@nom d'equipe</li>
-	</ul>
-</p>
- </footer>
+        <footer>
+            <ul>
+            <li>email</li><br>
+            <li>tel num</li><br>
+            <li>@nom d'equipe</li>
+        </ul>
+        </footer>
+    </body>
 </html>
