c5908efdbf8481090231a9976ae2dce56d9d4d24
mehdi-Ragad <93908338+mehdi-Ragad@users.noreply.github.com>
2021-11-30 18:29:15 +0100

    Add files via upload

---
+++ nouvellePage.php
@@ -0,0 +1,53 @@
+<?php require_once "./php/libgames.php" ?>
+<!DOCTYPE html>
+<html lang="fr">
+
+    <head>
+        <meta charset="utf-8"/>
+        <title>Pay Respect</title>
+        <link rel="icon" type="image/png" href="./images/icone.png" />
+        <link rel="stylesheet" href="styles/mainStyleNouvellePage.css"/>
+    </head>
+
+    <body>
+        <header>
+            <h1>Pay Respect </h1>
+
+            <p>Trouvez plein de jeux sur cet incroyable site qui n’est en fait qu’un prototype réalisé par les bg de BTS SNIR !</p>
+            
+            <label for="Recherche">Recherche:</label>
+<input type="Recherche" id="Recherche" name="Recherche"
+       aria-label="Recherche">
+            
+        </header>
+
+        <a href="index.php"><button>Redirection</button></a>
+
+
+        <main>
+           
+
+            <div class="TestJeux">
+                <div class="Tqt_tqt">
+                    <details>
+                        <summary>
+                             <img class="imageJeu" src="./images/Battlefield_2042.jpg" alt="image test">
+                    <p>Battlefield 2042<span class="note"> <img src="./images/etoiles.png"></span></p>
+
+                        </summary>
+                    </div>
+                    </details>
+                   
+                </div>
+<div class="TestJeux">
+    <div class="Tqt_tqt">
+        <details>
+            <summary>
+                <img class="imageJeu" src="./images/ForzaHorizon5.jpg" alt="image test">
+                    <p>Battlefield 2042<span class="note"> <img src="./images/etoiles.png"></span></p>
+            </summary>
+        </details>
+    </div>
+
+</div>
+                
