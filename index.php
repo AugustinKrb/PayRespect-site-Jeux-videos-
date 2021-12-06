b9de7fde08775f0fe59d2c5dfa4defbe2d393e5f
AugustinKrb <93907781+AugustinKrb@users.noreply.github.com>
2021-12-06 08:25:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -123,7 +123,7 @@
 
         <footer>
             <h2>Pay Respect</h2>
-            <p>Augustin KRABANSKY - Medhi Ragad - Wassim</p>
+            <p>Augustin KRABANSKY - Mehdi Ragad - Wassim</p>
         </footer>
     </body>
 </html>
