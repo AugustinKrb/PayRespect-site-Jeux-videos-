a2dd104469ad75ce8d2d95d5d275df43db3ad279
AugustinKrb <93907781+AugustinKrb@users.noreply.github.com>
2021-12-18 17:44:14 +0100

    Ajout ajout avis (pas encore possible)

--- pages/affichageJeu.php
+++ pages/affichageJeu.php
@@ -6,8 +6,28 @@
     $jeuChoisi = getGame($idJeu);
 
     //Test  AJOUTER UN USER APRES L'IP
-    //rateGame(51, "123", 8, "J'aime ce jeu, il est vraiment bien !");
-    //rateGame(51, "124", 4, "Bof...");
+    /*
+    rateGame(5, "123", "userTest", 8, "J'aime ce jeu, il est vraiment bien !");
+    rateGame(5, "124", "Haters", 4, "Bof...");
+    */
+    
+    /*
+    if (hasAlreadyRated(5, "124")) {
+        echo("<br><br><br>noté");
+    }
+
+    if (!hasAlreadyRated(5, "124")) {
+        echo("<br><br><br>pas noté");
+    }
+    */
+    
+    echo("<br><br><br>True : ".true);
+    echo("<br><br><br>False : ".false);
+    echo("<br><br><br>Avis déjà ajouté : ".hasAlreadyRated(5, "120"));
+    echo("<br><br><br>Avis pas encore ajouté :".hasAlreadyRated(5, "124"));
+
+    if (!empty($_REQUEST['ajouterAvis'])) {
+    }
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -75,9 +95,41 @@
                         </td>
                     </tr>
                 </table>
+
                 <div id="avisUtilisateurs">
+                    <?php
+                        var_dump(getGameComments(5));
+                        foreach (getGameComments(5) as $avis) {
+                            echo("ip : ".$avis['ip']." user : ".$avis['user']." note : ".$avis['note']." comment : ".$avis['comment'].".<br>");
+                        }
+                    ?>
                     <h1>Avis</h1>
-                    <table class="avisUnique">
+
+                    <table id="ajouterAvis">
+                        <form action="./affichageJeu.php">
+                            <tr>
+                                <td id="nePasAfficherIdJeu">
+                                    <input type="text" name="id" value="<?php echo($idJeu) ?>">
+                                </td>
+                                <td id="nomUserAjoutAvis">
+                                    <input type="text" name="nomUserAvis" placeholder="Nom d'utilisateur" maxlength="20" required>
+                                </td>
+                                <td id="avisUserAjoutAvis">
+                                    <textarea name="avisUserAvis" placeholder="Avis" rows="2" cols="100" required></textarea>
+                                </td>
+                                <td id="noteUserAjoutAvis">
+                                    <input type="number" name="noteUserAvis" min="0" max="10" placeholder="Note" required>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td id="boutonAjouterAvis" colspan="3">
+                                    <input type="submit" name="ajouterAvis" value="Ajouter un avis">
+                                </td>
+                            </tr>
+                        </form>
+                    </table>
+                    
+                    <table id="avisUnique">
                         <tr>
                             <td class="nomUserAvis">AntonioDu93</td>
                             <td class="avisUserAvis">Avis feijoijoij oijoz joi</td>
@@ -102,6 +154,11 @@
                             <td class="nomUserAvis">Police332789</td>
                             <td class="avisUserAvis">Sympa mais sans plus...</td>
                             <td class="noteUserAvis">5/10</td>
+                        </tr>
+                        <tr>
+                            <td class="nomUserAvis">Test</td>
+                            <td class="avisUserAvis">J'arrive même pas à comprendre qui peut aimer.ofs ojsold jofsjd ojiosj osjidf ojsdof ijsod joj ojs fojosdf josd jfoj ojd osdjo jsojs odsj o</td>
+                            <td class="noteUserAvis">0/10</td>
                         </tr>
                     </table>
                 </div>
