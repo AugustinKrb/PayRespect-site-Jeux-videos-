25748acd58d8cb9577809256c261840decd97aa7
AugustinKrb <93907781+AugustinKrb@users.noreply.github.com>
2021-12-18 15:26:55 +0100

    Changement chemin page recherche simple

--- pages/admin/admin.php
+++ pages/admin/admin.php
@@ -151,7 +151,7 @@
         <header>
             <h1>Pay Respect </h1>
             
-            <form action="./recherche.php" target="_blank">
+            <form action="../recherche.php" target="_blank">
                 <label for="recherche">Recherche :</label>
                 <input id="recherche" type="search" name="recherche" required>
                 <input  id="boutonRecherche"type="submit" value="Rechercher">
